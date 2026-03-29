5eaf7b7fe3b64b2a6a8d4e79af8a143c569427d1
honeycombnaveen <honeycombnaveen@gmail.com>
2026-03-29 19:47:20 +0530

    small changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,57 @@
 import { useState } from "react";
+import loginImg from "./assets/images/login.jpg";
 
 function App() {
-  const [email,setEmail]=useState("");
-  const [password,setpassword]=useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleLogin = () => {
+    if (!email || !password) {
+      setError("All fields are required");
+    } else if (!email.includes("@")) {
+      setError("Enter a valid email");
+    } else if (password.length < 6) {
+      setError("Password must be at least 6 characters");
+    } else {
+      setError("");
+      alert("Login successful");
+    }
+  };
+
   return (
     <div className="container">
-      <div className="form">
-        <h2>Login page</h2>
+      <div className="form " style={{
+    backgroundImage: `url(${loginImg})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+  }}>
+        
+        <div className="form-overlay">
+          <h2>Login</h2>
+        </div>
 
-        <input type="email"
-         placeholder="Enter Email" 
-         value={email}
-         onChange={(e)=> setEmail(e.target.value)}
-         />
+        {error && <p className="error">{error}</p>}
 
-         <input
+        <input
+          type="email"
+          placeholder="Enter Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <input
           type="password"
           placeholder="Enter Password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
 
-        <button>Login</button>
-
-
+        <button onClick={handleLogin}>Login</button>
       </div>
-      </div>
+    </div>
   );
 }
+
 export default App;
